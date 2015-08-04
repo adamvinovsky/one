817876ca2548efcfe6951974bd54588646c75ddf
Adam Vínovský <adamvinovsky@Adams-Mac.local>
2015-08-04 15:14:03 -0700

    Initial Commit

---
+++ one/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  one
+//
+//  Created by Adam Vínovský on 04/08/15.
+//  Copyright (c) 2015 Adam Vínovský. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ one/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  one
+//
+//  Created by Adam Vínovský on 04/08/15.
+//  Copyright (c) 2015 Adam Vínovský. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
